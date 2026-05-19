f32f8df1afe59eed025b0630346e97a8e67d145a
JvPortilho <82305660+JvPortilho@users.noreply.github.com>
2026-05-19 13:26:12 -0300

    Update notificador.py

--- services/notificador.py
+++ services/notificador.py
@@ -1,1 +1,22 @@
+# Notificador: emitir notificações do sistema.
 
+class Notificador:
+
+    def enviar_confirmacao(self, email, data_devolucao):
+
+        mensagem = (
+            f"Reserva registrada. "
+            f"Entrega prevista: {data_devolucao}"
+        )
+
+        self.enviar_email(mensagem)
+
+    def enviar_aviso_atraso(self, email):
+
+        mensagem = "Existe um empréstimo pendente em atraso."
+
+        self.enviar_email(mensagem)
+
+    def enviar_email(self, mensagem):
+
+        print(f"[EMAIL] {mensagem}")
