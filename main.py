5529b9fe2c0f1daba6de700d324af328935ee74f
JvPortilho <82305660+JvPortilho@users.noreply.github.com>
2026-05-19 13:28:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Estrutura inicial do sistema
+# main: iniciar funcionamento do sistema.
+
+from services.servico_emprestimo import (
+    ServicoEmprestimo
+)
+
+servico = ServicoEmprestimo()
+
+servico.registrar_emprestimo(
+    1,
+    "joao",
+    "jvportilho321@email.com",
+    5
+)
+
+print(servico.verificar_atrasos())
