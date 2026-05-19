00f12039c8a8901859e35283e39d0bbfc305f90e
JvPortilho <82305660+JvPortilho@users.noreply.github.com>
2026-05-19 13:24:11 -0300

    Update equipamento.py

--- models/equipamento.py
+++ models/equipamento.py
@@ -1,1 +1,29 @@
+# Equipamento: representar equipamentos cadastrados.
 
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Equipamento(ABC):
+    id: int
+    nome: str
+    disponivel: bool
+
+    @abstractmethod
+    def calcular_multa(self, dias_atraso):
+        pass
+
+
+@dataclass
+class Camera(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return max(0, dias_atraso * 8)
+
+
+@dataclass
+class CaixaSom(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return max(0, dias_atraso * 4)
