255e85a112e52ea255b29993ed8deb6a7ef66420
JvPortilho <82305660+JvPortilho@users.noreply.github.com>
2026-05-22 11:50:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
 # main: iniciar funcionamento do sistema.
 
+from repositories.repositorio_emprestimo import (
+    RepositorioEmprestimo
+)
+
+from services.notificador import (
+    Notificador
+)
+
 from services.servico_emprestimo import (
     ServicoEmprestimo
 )
 
-servico = ServicoEmprestimo()
 
-servico.registrar_emprestimo(
-    1,
-    "joao",
-    "jvportilho321@email.com",
-    5
-)
+def main():
 
-print(servico.verificar_atrasos())
+    repositorio = (
+        RepositorioEmprestimo()
+    )
+
+    notificador = (
+        Notificador()
+    )
+
+    servico = ServicoEmprestimo(
+        repositorio,
+        notificador
+    )
+
+    while True:
+
+        print(
+            "\n1-Registrar  "
+            "2-Devolver  "
+            "3-Atrasos  "
+            "0-Sair"
+        )
+
+        opcao = input("Opção: ")
+
+        if opcao == "1":
+
+            servico.registrar_emprestimo(
+                int(
+                    input(
+                        "ID equipamento: "
+                    )
+                ),
+                input("Nome: "),
+                input("Email: "),
+                int(input("Dias: "))
+            )
+
+        elif opcao == "2":
+
+            servico.finalizar_emprestimo(
+                int(
+                    input(
+                        "ID empréstimo: "
+                    )
+                )
+            )
+
+        elif opcao == "3":
+
+            servico.verificar_atrasos()
+
+        elif opcao == "0":
+            break
+
+
+if __name__ == "__main__":
+    main()
